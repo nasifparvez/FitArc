9dfff3d2f76f1c18aebda637ba3d2245f6e2e456
Mohammad Nasif Parvez <nasif.parvez2000@gmail.com>
2023-02-13 20:37:55 -0500

    edited slightly

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,48 +17,6 @@ function HomePage() {
           Easy
         </h1>
       </div>
-      <div className='infoSection'>
-      <div className='featuresSection'>
-        <div>
-          <h1 className='featuresTitle'>Features</h1>
-        </div>
-        <div className='featuresInfo'>
-        <p className='paragraph'>
-        FitArc is a one-stop web application that allows you to track your fitness and nutrition.
-        </p>
-        <div className='space'></div>
-        <img className='feautureImg' src={require('../images/features.png')} width={200} height={200}></img>
-        </div>
-        
-      </div>
-      <div className='funcSection'>
-      <div className='nutritionSection'>
-        <h2>Our Nutrition Page</h2>
-        <div>
-        
-        <div className='featuresInfo'>
-        <p className='paragraph'>
-         Get meal plans perfectly catered for your needs!
-        </p>
-        <div className='space'></div>
-        <img src={require('../images/nutrition.png')} width={200} height={200}></img>
-        </div> 
-        
-        </div>
-
-      </div>
-      <div className='fitnessSection'>
-        <h2>Our Fitness Page</h2>
-        <div className='featuresInfo'>
-        <p className='paragraph'>
-        Find and track workouts suited for your fitness jouney!
-        </p>
-        <div className='space'></div>
-        <img src={require('../images/weights.png')} width={200} height={200}></img>
-        </div> 
-      </div>
-      </div>
-      </div>
     </div>
     )
 }
